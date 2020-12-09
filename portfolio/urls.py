6acf0cb60fc9e7c980f757f30f0d125373af6016
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-12-09 21:01:30 +0600

    done with url and view of the skill page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from .views import skillsAndAbout
+
 app_name = "portfolio"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", skillsAndAbout.as_view(), name="skillsAndAbout"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,14 +6,41 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Web_Project, ML_Project
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 # Create your views here.
 def home(request):
     web_projects = Web_Project.objects.all()
     ml_projects = ML_Project.objects.all()
+    context = {"web_projects": web_projects, "ml_projects": ml_projects}
 
-    return render(
-        request,
-        "portfolio/home.html",
-        {"web_projects": web_projects, "ml_projects": ml_projects},
-    )
+    return render(request, "portfolio/home.html", context)
+
+
+# We can use multiple models like this. this is my sample code.
+
+# class about(ListView):
+#     model = Web_Project
+#     template_name = "portfolio/temp.html"
+#     # queryset = Web_Project.objects.all()
+
+#     def get_queryset(self):
+#         return Web_Project.objects.all()
+
+#     def get_context_data(self, **kwargs):
+#         context = super(ListView, self).get_context_data(**kwargs)
+#         context["web_projects"] = Web_Project.objects.all()
+#         context["ml_projects"] = ML_Project.objects.all()
+#         # And so on for more models
+#         return context
+
+
+class skillsAndAbout(ListView):
+    template_name = "portfolio/skillsAndAbout.html"
+    queryset = Web_Project.objects.all()
