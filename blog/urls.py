5c98adc04d80ce49dfa82a506e2c40f8041ffd51
Zenhar-Prodipto <zenhar.mz@gmail.com>
2021-03-04 16:26:56 +0600

    Added onScroll Animation

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ app_name = "blog"
 
 urlpatterns = [
     # Authentication
-    path("signup/", views.signupUser, name="signupUser"),  # Signup user er url
-    path("logout/", views.logoutUser, name="logoutUser"),  # logout user er url
-    path("login/", views.loginUser, name="loginUser"),  # login user er url
+    # path("signup/", views.signupUser, name="signupUser"),  # Signup user er url
+    # path("logout/", views.logoutUser, name="logoutUser"),  # logout user er url
+    # path("login/", views.loginUser, name="loginUser"),  # login user er url
     # Blog
     path("", views.home, name="home"),
     path("<int:blog_id>/", views.details, name="details"),
@@ -19,18 +19,16 @@ urlpatterns = [
         name="all_blogs_ascending",
     ),
     # ...
-    path("userhome/", views.userhome, name="userhome"),
-    path("create/", views.createBlogs, name="createBlogs"),  # Create new blogs
-    path("userhome/<int:user_blog_id>/", views.userBlogDetails, name="userBlogDetails"),
-    path("userhome/userallblogs/", views.user_all_blogs, name="user_all_blogs"),
-    path(
-        "userhome/userallblogs/userallblogsascending/",
-        views.user_all_blogs_ascending,
-        name="user_all_blogs_ascending",
-    ),
-    # ...
-    path("userhome/<int:user_blog_id>/delete", views.deleteblog, name="deleteblog"),
-    path("userhome/<int:user_blog_id>/edit", views.editblog, name="editblog"),
-    path("random/", views.random, name="random"),
-    path("random/again/", views.again, name="again"),
+    # path("userhome/", views.userhome, name="userhome"),
+    # path("create/", views.createBlogs, name="createBlogs"),  # Create new blogs
+    # path("userhome/<int:user_blog_id>/", views.userBlogDetails, name="userBlogDetails"),
+    # path("userhome/userallblogs/", views.user_all_blogs, name="user_all_blogs"),
+    # path(
+    #     "userhome/userallblogs/userallblogsascending/",
+    #     views.user_all_blogs_ascending,
+    #     name="user_all_blogs_ascending",
+    # ),
+    # # ...
+    # path("userhome/<int:user_blog_id>/delete", views.deleteblog, name="deleteblog"),
+    # path("userhome/<int:user_blog_id>/edit", views.editblog, name="editblog"),
 ]
