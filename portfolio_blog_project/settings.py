d1af66031b427942eef643065fdea1b624dae9b9
Zenhar-Prodipto <zenhar.mz@gmail.com>
2021-04-25 13:59:49 +0600

    Deployment

--- portfolio_blog_project/settings.py
+++ portfolio_blog_project/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["zenhar.herokuapp.com", "127.0.0.1"]
 
